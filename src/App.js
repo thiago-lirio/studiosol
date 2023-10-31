3f16a33aeec63e5aa9a7e9051e814e938505e2bc
Thiago Lirio <121976880+thiago-lirio@users.noreply.github.com>
2023-10-31 19:51:11 -0300

    Consumo de API com GraphQL e montagem do cabeçalho

--- src/App.js
+++ src/App.js
@@ -2,126 +2,183 @@ import { FaSistrix } from "react-icons/fa6";
 import { IoAddCircleSharp } from "react-icons/io5";
 import { IoHeartCircleSharp } from "react-icons/io5";
 import { TbLetterT } from "react-icons/tb";
-import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery } from '@apollo/client';
-
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+  useQuery,
+} from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'https://us-central1-ss-devops.cloudfunctions.net/GraphQL',
-  cache: new InMemoryCache()
-})
+  uri: "https://us-central1-ss-devops.cloudfunctions.net/GraphQL",
+  cache: new InMemoryCache(),
+});
 
 const GET_FAVORITE_BOOKS = gql`
-  query MinhaQuery{
-    favoriteBooks{
+  query FavoriteBooks {
+    favoriteBooks {
       name
       author {
         id
       }
       cover
-
     }
   }
-`
-const ContainerFavoriteBooks = ({children}) => (
-  <div class="favoriteBooks">{children}</div>
-)
+`;
+function ContainerFavoriteBooks() {
+  const { loading, error, data } = useQuery(GET_FAVORITE_BOOKS);
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
 
+  return data.favoriteBooks.map(({ name, cover }) => (
+    <div>
+      <h3>{name}</h3>
+      <img alt="location-reference" src={`${cover}`} />
+      <br />
+    </div>
+  ));
+}
 
+const GET_FAVORITE_ARTISTS = gql`
+  query FavoriteArtists {
+    favoriteAuthors {
+      name
+      picture
+    }
+  }
+`;
+function ContainerFavoriteArtists() {
+  const { loading, error, data } = useQuery(GET_FAVORITE_ARTISTS);
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return data.favoriteAuthors.map(({ name, picture }) => (
+    <div>
+      <h3>{name}</h3>
+      <img alt="location-reference" src={`${picture}`} />
+      <br />
+    </div>
+  ));
+}
+
+const GET_ALL_BOOKS = gql`
+  query AllBooks {
+    allBooks {
+      name
+      author {
+        id
+      }
+      cover
+    }
+  }
+`;
+
+function ContainerAllBooks() {
+  const { loading, error, data } = useQuery(GET_ALL_BOOKS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return data.allBooks.map(({ name, cover }) => (
+    <div>
+      <h3>{name}</h3>
+      <img alt="location-reference" src={`${cover}`} />
+      <br />
+    </div>
+  ));
+}
 
 function App() {
-  const {loading, data} = useQuery(GET_FAVORITE_BOOKS)
-  
-  if(loading) return <ContainerFavoriteBooks>Carregando...</ContainerFavoriteBooks>
-  
-  
-
   return (
     <div className="App">
       <ApolloProvider client={client}>
-
-      
-      <header className="App-header">
-       <h1 class="logo">SS Book</h1>
-        <div class="searcher">
-            <input type="text" class="txtSearcher" placeholder="Busque um livro"/>
-            <button><FaSistrix/></button>
-        </div>
-        <div class="add">
-          <IoAddCircleSharp/>
-          <h6>Adicionar</h6>
-        </div>
-        <div class="favorite">
-          <IoHeartCircleSharp/>
-          <h6>Favoritos</h6>
-        </div>
-        <div class="profile">
-          <TbLetterT/>
-          <h6>Thiago</h6>
-        </div>
-      </header>
-      <main>
-        <section class="tabs">
-          <ul>
-            <li>
-              <h5>Meus Livros</h5>    
-            </li>
-            <li>
-              <h5>Emprestados</h5>
-            </li>
-          </ul>
-        </section>
-        <section class="favoriteBooks">
-          <h3>Livros Favoritos</h3>
-          <h6>ver todos</h6>
-          <ContainerFavoriteBooks>
-            {data.favoriteBooks.map((item, index) => (
-              <div class="coverFavoriteBooks">
-                <img src="item.cover"></img>
-                <div class="nameFavoriteBooks">
-                  <h5>item.name</h5>
-                </div>
-                <div class="authorFavoriteBooks">
-                  <h5>item.author.id</h5>
-                </div>
+        <div class="container-fluid">
+          <header className=" App-header">
+            <div class="row d-flex  align-items-center justify-content-center ">
+              <h1 class="col  align-items-right">
+                SS<span>BOOK</span>
+              </h1>
+              <div
+                class="col-6 d-flex justify-content-center align-items-center"
+                id="searcher"
+              >
+                <input
+                  type="text"
+                  class="col txtSearcher"
+                  placeholder="Busque um livro"
+                />
+                <button class="col-1 align-items-right">
+                  <FaSistrix class="icons" />
+                </button>
               </div>
-              
-            )
-              
-            )}
-          </ContainerFavoriteBooks>
-          {/* Desenvolver lista com a API*/}
-        </section>
-        <div class="favoriteArtistsAndLibrary">
-          <section class="favoriteArtists">
-            <h3>Artistas Favoritos</h3>
-            {/* Desenvolver lista com a API*/}
-          </section>
-          <section class="Library">
-            <h3>Biblioteca</h3>
-            <ul>
-              <li>Todos</li>
-              <li>Romance</li>
-              <li>Aventura</li>
-              <li>Comédia</li>
-            </ul>
-            {/*Desenvolver lista com a API*/}
-          </section>
+              <div class="col d-flex align-items-center justify-content-center ">
+                <IoAddCircleSharp class="icons" />
+                <h6>Adicionar</h6>
+              </div>
+              <div class="col d-flex align-items-center justify-content-center ">
+                <IoHeartCircleSharp class="icons" />
+                <h6>Favoritos</h6>
+              </div>
+              <div
+                class="col d-flex align-items-center justify-content-center"
+                id="username"
+              >
+                <TbLetterT class="icons " id="photoUser" />
+                <h6>Thiago</h6>
+              </div>
+            </div>
+          </header>
         </div>
-      </main>
-      <footer>  
-        <h1 class="logo">SS Book</h1>
-        <div>
-          <h6>Todos os direitos reservados.</h6>
-          <h6>Studio Sol Books 2023</h6>
-        </div>
-        
-      </footer>
+        <main>
+          <nav class="nav nav-justified nav-fill">
+            <div class="container-fluid">
+              <div class="row d-flex align-items-center justify-content-center ">
+                <ul>
+                  <li class="col-1 d-flex align-items-center justify-content-center">
+                    <h5>Meus Livros</h5>
+                  </li>
+                  <li class="col-1 d-flex align-items-center justify-content-center">
+                    <h5>Emprestados</h5>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+          <section class="favoriteBooks">
+            <h3>Livros Favoritos</h3>
+            <h6>ver todos</h6>
+            <ContainerFavoriteBooks />
+          </section>
+          <div class="favoriteArtistsAndLibrary">
+            <section class="favoriteArtists">
+              <h3>Artistas Favoritos</h3>
+              <ContainerFavoriteArtists />
+            </section>
+            <section class="Library">
+              <h3>Biblioteca</h3>
+              <ul>
+                <li>Todos</li>
+                <li>Romance</li>
+                <li>Aventura</li>
+                <li>Comédia</li>
+              </ul>
 
+              <ContainerAllBooks />
+            </section>
+          </div>
+        </main>
+        <footer>
+          <h1 class="logo">SS Book</h1>
+          <div>
+            <h6>Todos os direitos reservados.</h6>
+            <h6>Studio Sol Books 2023</h6>
+          </div>
+        </footer>
       </ApolloProvider>
     </div>
-    
   );
 }
 
